4c008bf499c1e4a2c8ceefde9418e669438d74f7
Kc <40486366+cpwillis@users.noreply.github.com>
2024-02-16 13:40:27 +1000

    rm existing dir if exists, check for user confirmation

--- install.sh
+++ install.sh
@@ -6,6 +6,26 @@ REPO_URL="https://github.com/cpwillis/new-computer.git"
 # Define the directory where you want to clone the repository
 INSTALL_DIR="$HOME/Downloads/new-computer"
 
+# If the installation directory already exists, prompt the user to remove it
+if [ -d "$INSTALL_DIR" ]; then
+    printf "The installation directory already exists at %s.\n" "$INSTALL_DIR"
+    printf "Do you want to remove it and proceed with the installation? (y/n) "
+    read -r REPLY
+    case "$(printf "%s" "$REPLY" | tr '[:upper:]' '[:lower:]')" in
+    y | yes)
+        rm -rf "$INSTALL_DIR" || {
+            echo "Failed to remove the existing directory. Check your permissions." >&2
+            exit 1
+        }
+        printf "Existing installation directory removed successfully.\n"
+        ;;
+    *)
+        echo "Installation cancelled by the user."
+        exit 1
+        ;;
+    esac
+fi
+
 # Clone the repository
 echo "Cloning repository..."
 git clone "$REPO_URL" "$INSTALL_DIR" || { echo "Failed to clone repository"; exit 1; }
